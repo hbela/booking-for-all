06dfd19b146cb69d4275744806c93db7454bce9b
Bela Hajzer <hajzerbela@gmail.com>
2025-12-06 11:23:48 +0100

    fix: changes in gradle config

---
+++ apps/mobile/react-native.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    dependencies: {},
+    project: {
+      android: {
+        sourceDir: "./android",
+      },
+    },
+    reactNativePath: rmodule.exports = {
+  dependencies: {},
+  project: {
+    android: {
+      sourceDir: "./android",
+    },
+  },
+  reactNativePath: require.resolve("react-native"),
+  nodeModulesPath: [
+    // monorepo workspace roots where native deps located
+    require("path").join(__dirname, "../../node_modules"),
+    require("path").join(__dirname, "node_modules"),
+  ],
+};
+equire.resolve("react-native"),
+    nodeModulesPath: [
+      // monorepo workspace roots where native deps located
+      require("path").join(__dirname, "../../node_modules"),
+      require("path").join(__dirname, "node_modules"),
+    ],
+  };
+  
